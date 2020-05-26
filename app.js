52f61aeefad67f1b0e592a783969209e71757417
Matt Funk <mfunk017@gmail.com>
2020-05-26 12:30:16 -0500

    fixed stylesheet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static(__dirname + "public"));
+app.use(express.static('views'))
 
 app.use(bodyParser.urlencoded({
   extended: true
