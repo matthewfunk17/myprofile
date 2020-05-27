b7e850f772c172c8e7032e162a92fdc432243b15
Matt Funk <mfunk017@gmail.com>
2020-05-27 17:33:15 -0500

    travel page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const fs = require('fs');
 
 app.use(express.static("public"));
 
@@ -11,6 +12,17 @@ app.use(bodyParser.urlencoded({
 
 app.get("/", function(req, res) {
   res.render("index.ejs");
+});
+
+app.get("/travel", function(req, res) {
+  fs.readdir('./public/images', (error, files) => {
+    var imgFiles = [];
+    files.forEach(file => {
+      var imgpath = "images/" + file;
+      imgFiles.push(imgpath);
+    })
+  res.render('travel.ejs', {imgFiles: imgFiles});
+  })
 });
 
 app.listen(3000, function(){
